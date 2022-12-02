f428b63410c678e8068c026a7bb0fe62a488fdfc
Nima Pourjafar <nima.pourjafar123@gmail.com>
2022-12-02 13:25:01 -0500

    added openai

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import Head from "next/head";
+import Image from "next/image";
+import buildspaceLogo from "../assets/buildspace-logo.png";
+import { useState } from "react";
 
 const Home = () => {
+  const [userInput, setUserInput] = useState("");
+
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <Head>
@@ -11,14 +19,26 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>GPT-3 Dating-Coach</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Write a sentance about what you need, whether it be pick-up lines, rizz-tips, or a gift-idea for your girlfriend. </h2>
           </div>
           <div className="prompt-container">
-          <textarea placeholder="start typing here" className="prompt-box" />
-        </div>
+            <textarea
+              placeholder="start typing here"
+              className="prompt-box"
+              value={userInput}
+              onChange={onUserChangedText}
+            />
+          </div>
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={null}>
+              <div className="generate">
+                <p>Generate</p>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
       <div className="badge-container grow">
