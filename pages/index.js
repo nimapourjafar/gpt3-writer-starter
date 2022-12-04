37c000e7320c2cf1447015cb061fea371109e0a0
Nima Pourjafar <nima.pourjafar123@gmail.com>
2022-12-03 21:38:30 -0500

    finishsed next lesson

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,28 @@ import { useState } from "react";
 
 const Home = () => {
   const [userInput, setUserInput] = useState("");
+  const [apiOutput, setApiOutput] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...");
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text);
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  };
 
   const onUserChangedText = (event) => {
     console.log(event.target.value);
@@ -22,7 +44,10 @@ const Home = () => {
             <h1>GPT-3 Dating-Coach</h1>
           </div>
           <div className="header-subtitle">
-            <h2>Write a sentance about what you need, whether it be pick-up lines, rizz-tips, or a gift-idea for your girlfriend. </h2>
+            <h2>
+              Write a sentance about what you need, whether it be pick-up lines,
+              rizz-tips, or a gift-idea for your girlfriend.{" "}
+            </h2>
           </div>
           <div className="prompt-container">
             <textarea
@@ -33,11 +58,30 @@ const Home = () => {
             />
           </div>
           <div className="prompt-buttons">
-            <a className="generate-button" onClick={null}>
+            <a
+              className={
+                isGenerating ? "generate-button loading" : "generate-button"
+              }
+              onClick={callGenerateEndpoint}
+            >
               <div className="generate">
-                <p>Generate</p>
+                {isGenerating ? (
+                  <span className="loader"></span>
+                ) : (
+                  <p>Generate</p>
+                )}
               </div>
             </a>
+          </div>
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className="output-content">
+              <p>{apiOutput}</p>
+            </div>
           </div>
         </div>
       </div>
